023a2e4a216fca677495da87b1525de8bd310a5e
Cencendi <85205431+Cencendi@users.noreply.github.com>
2022-12-29 19:18:41 +0700

    edit urdf

--- coba.py
+++ coba.py
@@ -12,19 +12,32 @@ planeId = p.loadURDF("plane.urdf")
 startPos = [0, 0, 1.4054411813121799]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("aba_excavator/excavator.urdf",startPos, startOrientation)
-o = []
+o,o1 = [], []
+
+def normalize(x):
+    return ((x+np.pi)%(2*np.pi)) - np.pi
+
 for i in range(1000):
-    # p.setJointMotorControl2(boxId, 1 , p.POSITION_CONTROL, targetPosition = 0)
-    p.setJointMotorControl2(boxId, 2 , p.POSITION_CONTROL, targetPosition = -0.7, force= 250_000)
+    p.setJointMotorControl2(boxId, 1 , p.POSITION_CONTROL, targetPosition = 0, force=50000)
+    p.setJointMotorControl2(boxId, 2 , p.POSITION_CONTROL, targetPosition = -0.5, force= 250_000)
     p.setJointMotorControl2(boxId, 3 , p.POSITION_CONTROL, targetPosition = 1, force= 250_000)
-    p.setJointMotorControl2(boxId, 4 , p.POSITION_CONTROL, targetPosition = 1.57)
-    (linkWorldPosition, orientation, *_) = p.getLinkState(boxId,4, computeLinkVelocity=1, computeForwardKinematics=1)
+    p.setJointMotorControl2(boxId, 4 , p.VELOCITY_CONTROL, targetVelocity = np.pi, force=250_000)
+    (linkWorldPosition, orientation, *_) = p.getLinkState(boxId,5, computeLinkVelocity=1, computeForwardKinematics=1)
     # print(linkWorldPosition)    
     p.stepSimulation()
     time.sleep(1.0/240.)
-    # theta0, theta1, theta2, theta3 = p.getJointStates(boxId, [1,2,3,4])
-    # print(theta0[0], theta1[0], theta2[0], theta3[0])
-    print(orientation)
+    theta1 = p.getJointState(boxId,2)
+    theta2 = p.getJointState(boxId,3)
+    theta3 = p.getJointState(boxId,4)
+    print(linkWorldPosition)
+    ori = normalize(-(theta1[0]+theta2[0]+theta3[0]))
+    orientation = p.getEulerFromQuaternion(orientation)
+    print(orientation[1], ori)
     o.append(np.array(orientation))
-p.disconnect()
+    o1.append(ori)
+    if ori==-np.pi:
+        break
+# p.disconnect()
 plt.plot(list(range(1000)),np.array(o)[:,1])
+plt.plot(list(range(1000)),np.array(o1))
+plt.show()
